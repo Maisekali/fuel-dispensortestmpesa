f465c15ae009042db8639d0b659e2a5c245d6592
Maisekali <164651213+Maisekali@users.noreply.github.com>
2026-05-01 13:27:16 +0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+
+// ================= DARAJA SANDBOX CREDENTIALS =================
+
+// Your provided keys
+define("CONSUMER_KEY", "84G9G8DpJMN6oyylA89Uy50ZAi02CJ00a33vGIgcO7GB3INY");
+define("CONSUMER_SECRET", "dVHo3G2BAPIa7uehEsd8khtLECGCSWTP1xmf9DBousPPAEBPhhA4qh5f7TFCPUlx");
+
+// Sandbox Paybill
+define("SHORTCODE", "174379");
+
+// Sandbox Passkey (DO NOT CHANGE)
+define("PASSKEY", "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919");
+
+?>
